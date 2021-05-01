0d9d6de51c14669e57860dffce17efc0d75bd314
Stefan Hagen <stefan@hagen.link>
2021-05-01 23:15:47 +0200

    Added real main

--- csaf/__main__.py
+++ csaf/__main__.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long,missing-module-docstring
+import sys
 
+from csaf.cli import main
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))  # pragma: no cover
