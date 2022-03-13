1b16642b74c85669491c158cf0620d6d6a5b479c
Stefan Hagen <stefan@hagen.link>
2022-03-13 17:07:47 +0100

    Added top level CSAF model

--- csaf/csaf.py
+++ csaf/csaf.py
@@ -1,12 +1,57 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=c-extension-no-member,expression-not-assigned,line-too-long,logging-fstring-interpolation
-"""Do the lint."""
+"""CSAF Document model."""
+from __future__ import annotations
+
 import json
 import pathlib
-from typing import Mapping, Sequence, Tuple, no_type_check
+from typing import Annotated, List, Mapping, Optional, Sequence, Tuple, no_type_check
+
+from pydantic import BaseModel, Field, validator
 
 import csaf
 from csaf import log
+from csaf.document import Document
+from csaf.product import ProductTree
+from csaf.vulnerability import Vulnerability
+
+
+class CSAF(BaseModel):
+    """
+    Representation of security advisory information as a JSON document.
+    """
+
+    document: Annotated[
+        Document,
+        Field(
+            description='Captures the meta-data about this document describing a particular set of'
+            ' security advisories.',
+            title='Document level meta-data',
+        ),
+    ]
+    product_tree: Annotated[
+        Optional[ProductTree],
+        Field(
+            description='Is a container for all fully qualified product names that can be referenced elsewhere'
+            ' in the document.',
+            title='Product tree',
+        ),
+    ]
+    vulnerabilities: Annotated[
+        Optional[List[Vulnerability]],
+        Field(
+            description='Represents a list of all relevant vulnerability information items.',
+            min_items=1,
+            title='Vulnerabilities',
+        ),
+    ]
+
+    @no_type_check
+    @validator('vulnerabilities')
+    def check_len(cls, v):
+        if not v:
+            raise ValueError('vulnerabilities present but empty')
+        return v
 
 
 def is_valid(path: str, options: Mapping[str, bool]) -> bool:
