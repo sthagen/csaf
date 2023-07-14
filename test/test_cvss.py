40b6db9880f6d1f07017607d110a0035f47b6552
Stefan Hagen <stefan@hagen.link>
2023-07-14 12:10:40 +0200

    Adapted expected schema to new pydantic schema presentation

--- test/test_cvss.py
+++ test/test_cvss.py
@@ -15,15 +15,15 @@ JSON = json.dumps(DATA)
 def test_cvss31_minimal():
     vector_string = 'CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:C/C:H/I:H/A:H'
     expected_value = (
-        f'{{"version": "3.1", "vectorString": "{vector_string}", "attackVector": null,'
-        ' "attackComplexity": null, "privilegesRequired": null, "userInteraction": null, "scope": null,'
-        ' "confidentialityImpact": null, "integrityImpact": null, "availabilityImpact": null, "baseScore": 10.0,'
-        ' "baseSeverity": "CRITICAL", "exploitCodeMaturity": null, "remediationLevel": null, "reportConfidence": null,'
-        ' "temporalScore": null, "temporalSeverity": null, "confidentialityRequirement": null,'
-        ' "integrityRequirement": null, "availabilityRequirement": null, "modifiedAttackVector": null,'
-        ' "modifiedAttackComplexity": null, "modifiedPrivilegesRequired": null, "modifiedUserInteraction": null,'
-        ' "modifiedScope": null, "modifiedConfidentialityImpact": null, "modifiedIntegrityImpact": null,'
-        ' "modifiedAvailabilityImpact": null, "environmentalScore": null, "environmentalSeverity": null}'
+        f'{{"version":"3.1","vectorString":"{vector_string}","attackVector":null,'
+        '"attackComplexity":null,"privilegesRequired":null,"userInteraction":null,"scope":null,'
+        '"confidentialityImpact":null,"integrityImpact":null,"availabilityImpact":null,"baseScore":10.0,'
+        '"baseSeverity":"CRITICAL","exploitCodeMaturity":null,"remediationLevel":null,"reportConfidence":null,'
+        '"temporalScore":null,"temporalSeverity":null,"confidentialityRequirement":null,'
+        '"integrityRequirement":null,"availabilityRequirement":null,"modifiedAttackVector":null,'
+        '"modifiedAttackComplexity":null,"modifiedPrivilegesRequired":null,"modifiedUserInteraction":null,'
+        '"modifiedScope":null,"modifiedConfidentialityImpact":null,"modifiedIntegrityImpact":null,'
+        '"modifiedAvailabilityImpact":null,"environmentalScore":null,"environmentalSeverity":null}'
     )
     c31 = cvss.CVSS31.model_validate_json(JSON)
     assert c31.json() == expected_value
@@ -39,7 +39,7 @@ def test_cvss31_minimal():
         'title': 'CVSS31',
         'type': 'object',
         'properties': {
-            'version': {'description': 'CVSS Version', 'default': '3.1', 'allOf': [{'$ref': '#/definitions/Version'}]},
+            'version': {'description': 'CVSS Version', 'default': '3.1', 'allOf': [{'$ref': '#/$defs/Version'}]},
             'vectorString': {
                 'title': 'Vectorstring',
                 'pattern': (
@@ -50,115 +50,206 @@ def test_cvss31_minimal():
                 ),
                 'type': 'string',
             },
-            'attackVector': {'$ref': '#/definitions/AttackVectorType'},
-            'attackComplexity': {'$ref': '#/definitions/AttackComplexityType'},
-            'privilegesRequired': {'$ref': '#/definitions/PrivilegesRequiredType'},
-            'userInteraction': {'$ref': '#/definitions/UserInteractionType'},
-            'scope': {'$ref': '#/definitions/ScopeType'},
-            'confidentialityImpact': {'$ref': '#/definitions/CiaType'},
-            'integrityImpact': {'$ref': '#/definitions/CiaType'},
-            'availabilityImpact': {'$ref': '#/definitions/CiaType'},
-            'baseScore': {'$ref': '#/definitions/ScoreType'},
-            'baseSeverity': {'$ref': '#/definitions/SeverityType'},
-            'exploitCodeMaturity': {'$ref': '#/definitions/ExploitCodeMaturityType'},
-            'remediationLevel': {'$ref': '#/definitions/RemediationLevelType'},
-            'reportConfidence': {'$ref': '#/definitions/ConfidenceType'},
-            'temporalScore': {'$ref': '#/definitions/ScoreType'},
-            'temporalSeverity': {'$ref': '#/definitions/SeverityType'},
-            'confidentialityRequirement': {'$ref': '#/definitions/CiaRequirementType'},
-            'integrityRequirement': {'$ref': '#/definitions/CiaRequirementType'},
-            'availabilityRequirement': {'$ref': '#/definitions/CiaRequirementType'},
-            'modifiedAttackVector': {'$ref': '#/definitions/ModifiedAttackVectorType'},
-            'modifiedAttackComplexity': {'$ref': '#/definitions/ModifiedAttackComplexityType'},
-            'modifiedPrivilegesRequired': {'$ref': '#/definitions/ModifiedPrivilegesRequiredType'},
-            'modifiedUserInteraction': {'$ref': '#/definitions/ModifiedUserInteractionType'},
-            'modifiedScope': {'$ref': '#/definitions/ModifiedScopeType'},
-            'modifiedConfidentialityImpact': {'$ref': '#/definitions/ModifiedCiaType'},
-            'modifiedIntegrityImpact': {'$ref': '#/definitions/ModifiedCiaType'},
-            'modifiedAvailabilityImpact': {'$ref': '#/definitions/ModifiedCiaType'},
-            'environmentalScore': {'$ref': '#/definitions/ScoreType'},
-            'environmentalSeverity': {'$ref': '#/definitions/SeverityType'},
+            'attackVector': {
+                'anyOf': [{'$ref': '#/$defs/AttackVectorType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'attackComplexity': {
+                'anyOf': [{'$ref': '#/$defs/AttackComplexityType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'privilegesRequired': {
+                'anyOf': [{'$ref': '#/$defs/PrivilegesRequiredType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'userInteraction': {
+                'anyOf': [{'$ref': '#/$defs/UserInteractionType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'scope': {
+                'anyOf': [{'$ref': '#/$defs/ScopeType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'confidentialityImpact': {
+                'anyOf': [{'$ref': '#/$defs/CiaType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'integrityImpact': {
+                'anyOf': [{'$ref': '#/$defs/CiaType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'availabilityImpact': {
+                'anyOf': [{'$ref': '#/$defs/CiaType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'baseScore': {'$ref': '#/$defs/ScoreType'},
+            'baseSeverity': {'$ref': '#/$defs/SeverityType'},
+            'exploitCodeMaturity': {
+                'anyOf': [{'$ref': '#/$defs/ExploitCodeMaturityType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'remediationLevel': {
+                'anyOf': [{'$ref': '#/$defs/RemediationLevelType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'reportConfidence': {
+                'anyOf': [{'$ref': '#/$defs/ConfidenceType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'temporalScore': {
+                'anyOf': [{'$ref': '#/$defs/ScoreType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'temporalSeverity': {
+                'anyOf': [{'$ref': '#/$defs/SeverityType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'confidentialityRequirement': {
+                'anyOf': [{'$ref': '#/$defs/CiaRequirementType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'integrityRequirement': {
+                'anyOf': [{'$ref': '#/$defs/CiaRequirementType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'availabilityRequirement': {
+                'anyOf': [{'$ref': '#/$defs/CiaRequirementType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'modifiedAttackVector': {
+                'anyOf': [{'$ref': '#/$defs/ModifiedAttackVectorType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'modifiedAttackComplexity': {
+                'anyOf': [{'$ref': '#/$defs/ModifiedAttackComplexityType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'modifiedPrivilegesRequired': {
+                'anyOf': [{'$ref': '#/$defs/ModifiedPrivilegesRequiredType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'modifiedUserInteraction': {
+                'anyOf': [{'$ref': '#/$defs/ModifiedUserInteractionType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'modifiedScope': {
+                'anyOf': [{'$ref': '#/$defs/ModifiedScopeType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'modifiedConfidentialityImpact': {
+                'anyOf': [{'$ref': '#/$defs/ModifiedCiaType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'modifiedIntegrityImpact': {
+                'anyOf': [{'$ref': '#/$defs/ModifiedCiaType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'modifiedAvailabilityImpact': {
+                'anyOf': [{'$ref': '#/$defs/ModifiedCiaType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'environmentalScore': {
+                'anyOf': [{'$ref': '#/$defs/ScoreType'}, {'type': 'null'}],
+                'default': None,
+            },
+            'environmentalSeverity': {
+                'anyOf': [{'$ref': '#/$defs/SeverityType'}, {'type': 'null'}],
+                'default': None,
+            },
         },
         'required': ['vectorString', 'baseScore', 'baseSeverity'],
-        'definitions': {
-            'Version': {'title': 'Version', 'description': 'CVSS Version', 'enum': ['2.0', '3.0', '3.1']},
+        '$defs': {
+            'Version': {
+                'description': 'CVSS Version',
+                'enum': ['2.0', '3.0', '3.1'],
+                'title': 'Version',
+                'type': 'string',
+            },
             'AttackVectorType': {
-                'title': 'AttackVectorType',
-                'description': 'An enumeration.',
                 'enum': ['NETWORK', 'ADJACENT_NETWORK', 'LOCAL', 'PHYSICAL'],
+                'title': 'AttackVectorType',
+                'type': 'string',
             },
             'AttackComplexityType': {
-                'title': 'AttackComplexityType',
-                'description': 'An enumeration.',
                 'enum': ['HIGH', 'LOW'],
+                'title': 'AttackComplexityType',
+                'type': 'string',
             },
             'PrivilegesRequiredType': {
-                'title': 'PrivilegesRequiredType',
-                'description': 'An enumeration.',
                 'enum': ['HIGH', 'LOW', 'NONE'],
+                'title': 'PrivilegesRequiredType',
+                'type': 'string',
             },
             'UserInteractionType': {
-                'title': 'UserInteractionType',
-                'description': 'An enumeration.',
                 'enum': ['NONE', 'REQUIRED'],
+                'title': 'UserInteractionType',
+                'type': 'string',
             },
-            'ScopeType': {'title': 'ScopeType', 'description': 'An enumeration.', 'enum': ['UNCHANGED', 'CHANGED']},
-            'CiaType': {'title': 'CiaType', 'description': 'An enumeration.', 'enum': ['NONE', 'LOW', 'HIGH']},
+            'ScopeType': {
+                'enum': ['UNCHANGED', 'CHANGED'],
+                'title': 'ScopeType',
+                'type': 'string',
+            },
+            'CiaType': {
+                'enum': ['NONE', 'LOW', 'HIGH'],
+                'title': 'CiaType',
+                'type': 'string',
+            },
             'ScoreType': {'title': 'ScoreType', 'minimum': 0.0, 'maximum': 10.0, 'type': 'number'},
             'SeverityType': {
-                'title': 'SeverityType',
-                'description': 'An enumeration.',
                 'enum': ['NONE', 'LOW', 'MEDIUM', 'HIGH', 'CRITICAL'],
+                'title': 'SeverityType',
+                'type': 'string',
             },
             'ExploitCodeMaturityType': {
-                'title': 'ExploitCodeMaturityType',
-                'description': 'An enumeration.',
                 'enum': ['UNPROVEN', 'PROOF_OF_CONCEPT', 'FUNCTIONAL', 'HIGH', 'NOT_DEFINED'],
+                'title': 'ExploitCodeMaturityType',
+                'type': 'string',
             },
             'RemediationLevelType': {
-                'title': 'RemediationLevelType',
-                'description': 'An enumeration.',
                 'enum': ['OFFICIAL_FIX', 'TEMPORARY_FIX', 'WORKAROUND', 'UNAVAILABLE', 'NOT_DEFINED'],
+                'title': 'RemediationLevelType',
+                'type': 'string',
             },
             'ConfidenceType': {
-                'title': 'ConfidenceType',
-                'description': 'An enumeration.',
                 'enum': ['UNKNOWN', 'REASONABLE', 'CONFIRMED', 'NOT_DEFINED'],
+                'title': 'ConfidenceType',
+                'type': 'string',
             },
             'CiaRequirementType': {
-                'title': 'CiaRequirementType',
-                'description': 'An enumeration.',
                 'enum': ['LOW', 'MEDIUM', 'HIGH', 'NOT_DEFINED'],
+                'title': 'CiaRequirementType',
+                'type': 'string',
             },
             'ModifiedAttackVectorType': {
-                'title': 'ModifiedAttackVectorType',
-                'description': 'An enumeration.',
                 'enum': ['NETWORK', 'ADJACENT_NETWORK', 'LOCAL', 'PHYSICAL', 'NOT_DEFINED'],
+                'title': 'ModifiedAttackVectorType',
+                'type': 'string',
             },
             'ModifiedAttackComplexityType': {
-                'title': 'ModifiedAttackComplexityType',
-                'description': 'An enumeration.',
                 'enum': ['HIGH', 'LOW', 'NOT_DEFINED'],
+                'title': 'ModifiedAttackComplexityType',
+                'type': 'string',
             },
             'ModifiedPrivilegesRequiredType': {
-                'title': 'ModifiedPrivilegesRequiredType',
-                'description': 'An enumeration.',
                 'enum': ['HIGH', 'LOW', 'NONE', 'NOT_DEFINED'],
+                'title': 'ModifiedPrivilegesRequiredType',
+                'type': 'string',
             },
             'ModifiedUserInteractionType': {
-                'title': 'ModifiedUserInteractionType',
-                'description': 'An enumeration.',
                 'enum': ['NONE', 'REQUIRED', 'NOT_DEFINED'],
+                'title': 'ModifiedUserInteractionType',
+                'type': 'string',
             },
             'ModifiedScopeType': {
-                'title': 'ModifiedScopeType',
-                'description': 'An enumeration.',
                 'enum': ['UNCHANGED', 'CHANGED', 'NOT_DEFINED'],
+                'title': 'ModifiedScopeType',
+                'type': 'string',
             },
             'ModifiedCiaType': {
-                'title': 'ModifiedCiaType',
-                'description': 'An enumeration.',
                 'enum': ['NONE', 'LOW', 'HIGH', 'NOT_DEFINED'],
+                'title': 'ModifiedCiaType',
+                'type': 'string',
             },
         },
     }
