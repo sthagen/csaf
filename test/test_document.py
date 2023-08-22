5caab6df46ee5db05a9a34b6f8205ae5248bc969
Stefan Hagen <stefan@hagen.link>
2023-08-22 19:39:06 +0200

    added document meta tests

--- test/test_document.py
+++ test/test_document.py
@@ -19,6 +19,58 @@ def test_doc_empty_meta():
         assert f'\n{prop}\n  Field required' in str(err.value)
 
 
+def test_doc_meta_none():
+    with pytest.raises(ValidationError, match=_subs(5, 'Document')) as err:
+        _ = csaf.Document()  # type: ignore
+    for prop in ('category', 'csaf_version', 'publisher', 'title', 'tracking'):
+        assert f'\n{prop}\n  Field required' in str(err.value)
+
+
+def test_doc_meta_category_empty():
+    with pytest.raises(ValidationError, match=_subs(5, 'Document')) as err:
+        _ = csaf.Document(category='')  # type: ignore
+    hint = 'String should have at least 1 character'
+    assert f'\ncategory\n  {hint}' in str(err.value)
+    for prop in ('csaf_version', 'publisher', 'title', 'tracking'):
+        assert f'\n{prop}\n  Field required' in str(err.value)
+
+
+def test_doc_meta_csaf_version_wrong():
+    with pytest.raises(ValidationError, match=_subs(4, 'Document')) as err:
+        _ = csaf.Document(**conftest.META_WRONG_VERSION)  # type: ignore
+    hint = "Input should be '2.0'"
+    assert f'\ncsaf_version\n  {hint}' in str(err.value)
+    for prop in ('publisher', 'title', 'tracking'):
+        assert f'\n{prop}\n  Field required' in str(err.value)
+
+
+def test_doc_meta_publisher_empty():
+    with pytest.raises(ValidationError, match=_subs(5, 'Document')) as err:
+        _ = csaf.Document(**conftest.META_EMPTY_PUBLISHER)  # type: ignore
+    for prop in ('title', 'tracking'):
+        assert f'\n{prop}\n  Field required' in str(err.value)
+    host = 'publisher'
+    for prop in ('category', 'name', 'namespace'):
+        assert f'\n{host}.{prop}\n  Field required' in str(err.value)
+
+
+def test_doc_meta_title_empty():
+    with pytest.raises(ValidationError, match=_subs(2, 'Document')) as err:
+        _ = csaf.Document(**conftest.META_EMPTY_TITLE)  # type: ignore
+    hint = 'String should have at least 1 character'
+    assert f'\ntitle\n  {hint}' in str(err.value)
+    for prop in ('tracking',):
+        assert f'\n{prop}\n  Field required' in str(err.value)
+
+
+def test_doc_meta_tracking_empty():
+    with pytest.raises(ValidationError, match=_subs(6, 'Document')) as err:
+        _ = csaf.Document(**conftest.META_EMPTY_TRACKING)  # type: ignore
+    host = 'tracking'
+    for prop in ('current_release_date', 'id', 'initial_release_date', 'revision_history', 'status', 'version'):
+        assert f'\n{host}.{prop}\n  Field required' in str(err.value)
+
+
 def test_doc_ok_if_spammy():
     doc = csaf.CSAF(**conftest.DOC_OK)
     strip_me = msgspec.json.decode(doc.model_dump_json())
